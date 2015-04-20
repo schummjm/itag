aa9e380a9729e11d198da00ac46e2ca4afd22042
schummjm <jonathanschummer@gmail.com>
2015-04-20 16:40:02 -0500

    update script

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -12,24 +12,11 @@
 jQuery(document).ready(function() {
 	var webinar_id = {{ $id }};
 	var email = getURLParameter('email');
-	jQuery.ajax
-	({
-	  type: &quot;GET&quot;,
-	  url: &quot;http://itag.visiontechteam.com/api/start/&quot;+webinar_id+&quot;/&quot;+email+&quot;/&quot;,
-	  dataType: &quot;jsonp&quot;,
-	  async: false,
-	  crossDomain:true,
-	  jsonp: &quot;callback&quot;,
-	  success: function (data){
-	    console.log(data.success); 
-	  }
-	});
-	jQuery(window).bind('beforeunload', function(event) {
-	    	event.stopPropagation();
-	        jQuery.ajax
+	if (email != null) {
+		jQuery.ajax
 		({
 		  type: &quot;GET&quot;,
-		  url: &quot;http://itag.visiontechteam.com/api/end/&quot;+webinar_id+&quot;/&quot;+email+&quot;/&quot;,
+		  url: &quot;http://itag.visiontechteam.com/api/start/&quot;+webinar_id+&quot;/&quot;+email+&quot;/&quot;,
 		  dataType: &quot;jsonp&quot;,
 		  async: false,
 		  crossDomain:true,
@@ -38,7 +25,22 @@ jQuery(document).ready(function() {
 		    console.log(data.success); 
 		  }
 		});
-	});
+		jQuery(window).bind('beforeunload', function(event) {
+		    	event.stopPropagation();
+		        jQuery.ajax
+			({
+			  type: &quot;GET&quot;,
+			  url: &quot;http://itag.visiontechteam.com/api/end/&quot;+webinar_id+&quot;/&quot;+email+&quot;/&quot;,
+			  dataType: &quot;jsonp&quot;,
+			  async: false,
+			  crossDomain:true,
+			  jsonp: &quot;callback&quot;,
+			  success: function (data){
+			    console.log(data.success); 
+			  }
+			});
+		});
+	}
 	function getURLParameter(name) {
 	  return decodeURIComponent((new RegExp('[?|&amp;]' + name + '=' + '([^&amp;;]+?)(&amp;|#|;|$)').exec(location.search)||[,&quot;&quot;])[1].replace(/\+/g, '%20'))||null
 	}
