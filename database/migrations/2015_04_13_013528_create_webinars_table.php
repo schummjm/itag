d3d48c5ce7993855a878b5ea015f3dc308307822
schummjm <jonathanschummer@gmail.com>
2015-04-20 15:35:54 -0500

    db refresh

--- database/migrations/2015_04_13_013528_create_webinars_table.php
+++ database/migrations/2015_04_13_013528_create_webinars_table.php
@@ -19,7 +19,7 @@ class CreateWebinarsTable extends Migration {
 			$table->string('api_key');
 			$table->string('webinar_name');
 			$table->string('webinar_date');
-			$table->boolean('dst')->default(0);
+			$table->boolean('dst')->default(0)->nullable();
 			$table->timestamps();
 		});
 	}
